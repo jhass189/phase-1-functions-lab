1dc32fc73e44c65065e9ffa1c3ee19a8b61a7bc3
James Hassett <jhassett189@gmail.com>
2022-06-06 21:51:32 -0400

    commit tree 3a7e84ce40cf41d344349c54561158633494e2f9

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    console.log(start)
+    if (start < 42) {
+        return 42 - start
+    } else {
+        return start - 42
+    }
+}
+
+function distanceFromHqInFeet(start) {
+    distanceFromHqInBlocks(start);
+    if (start < 42) {
+        return (42 - start) * 264
+    } else {
+        return (start - 42) * 264
+    }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    console.log(distanceTravelledInFeet(start, destination))
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+        return (0)
+    } else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * .02
+
+
+
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
